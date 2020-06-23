78deae6fea600c4af9ab1a597ffad8020e5d94b7
Tanner <tdyer@hawk.iit.edu>
2020-06-23 12:34:35 -0500

    added phone number container

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,30 @@ class MyApp extends StatelessWidget {
                     color: Colors.red[200],
                     fontFamily: 'Source Sans Pro',
                   )
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.red[400], 
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '(847)-856-9873',
+                        style: TextStyle(
+                          color: Colors.red[400],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0
+                        ),
+                      )
+                    ],
+                  )
                 )
               ],
             ),
